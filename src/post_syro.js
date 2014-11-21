7bebe346b3e9ccfa300a492812e07c85b0f17e34
Panagiotis Tigas <ptigas@gmail.com>
2014-11-21 00:31:44 +0000

    beautiful

--- src/post_syro.js
+++ src/post_syro.js
@@ -1,67 +1,64 @@
-(function(window){
+(function(window) {
+    function intFromBytes(x, big_endian) {
+        var val = 0;
+        for (var i = 0; i < x.length; ++i) {
 
-    function intFromBytes( x, big_endian ){
-    var val = 0;
-    for (var i = 0; i < x.length; ++i) {    
+            if (big_endian) {
+                val += x[i];
+            } else {
+                val += x[x.length - i - 1];
+            }
 
-            if (big_endian)
-            {
-              val += x[i];
-            } 
-            else
-            {
-              val += x[x.length-i-1];          
-            }   
-            
-            if (i < x.length-1) {
+            if (i < x.length - 1) {
                 val = val << 8;
             }
         }
         return val;
     }
 
-    function uint8ArrayToArrayBuffer(buffer)
-{
-  var output = new ArrayBuffer(buffer.byteLength);
-  for (var i = 0; i < buffer.byteLength; ++i) {   
-    output[i] = buffer[i];
-  }
-  return output;
-}
+    function uint8ArrayToArrayBuffer(buffer) {
+        var output = new ArrayBuffer(buffer.byteLength);
+        for (var i = 0; i < buffer.byteLength; ++i) {
+            output[i] = buffer[i];
+        }
+        return output;
+    }
 
-  var Syrialize = function(audio, callback) {
-    syro = Module.cwrap(
-    'syrializer', 'number', ['number', 'number', 'number']
-    );
-    
-    var reader = new FileReader();
+    var Syrialize = function(audio, callback) {
+        syro = Module.cwrap(
+            'syrializer', 'number', ['number', 'number', 'number']
+        );
 
-    reader.onload = function() {
-      data = new Uint8Array(reader.result);    
+        var reader = new FileReader();
 
-      console.log("data=", data);
-      var nDataBytes = data.length * data.BYTES_PER_ELEMENT;
-      var dataPtr = Module._malloc(nDataBytes);
+        reader.onload = function() {
+            data = new Uint8Array(reader.result);
 
-      HEAPU8.set(data, dataPtr);    
-      
-      var size_dest_bytes = 4; // 32 unsigned is 4 bytes;
-      var size_dest_ptr = Module._malloc(size_dest_bytes);    
-      
-      var syralizedData = syro(dataPtr, size_dest_ptr, nDataBytes);
-      
-      var len = intFromBytes(HEAPU8.subarray(size_dest_ptr, size_dest_ptr+ 4), false);
+            console.log("data=", data);
+            var nDataBytes = data.length * data.BYTES_PER_ELEMENT;
+            var dataPtr = Module._malloc(nDataBytes);
 
-      var res = new Uint8Array(HEAPU8.subarray(syralizedData, syralizedData+len));
+            HEAPU8.set(data, dataPtr);
 
-      this.audio = new Blob(new Array(res.buffer), {type: blob.type});
+            var size_dest_bytes = 4; // 32 unsigned is 4 bytes;
+            var size_dest_ptr = Module._malloc(size_dest_bytes);
 
-      callback(this.audio);
-    };
+            var syralizedData = syro(dataPtr, size_dest_ptr, nDataBytes);
 
-    reader.readAsArrayBuffer(blob);    
-  }
+            var len = intFromBytes(HEAPU8.subarray(size_dest_ptr, size_dest_ptr + 4), false);
 
-  window.Syrialize = Syrialize;
+            var res = new Uint8Array(HEAPU8.subarray(syralizedData, syralizedData + len));
+
+            this.audio = new Blob(new Array(res.buffer), {
+                type: blob.type
+            });
+
+            callback(this.audio);
+        };
+
+        reader.readAsArrayBuffer(blob);
+    }
+
+    window.Syrialize = Syrialize;
 
 })(window);
